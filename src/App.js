330626304de4428460a0436299f4e62487e7b0d7
Amit <amitdhiman5086@gmail.com>
2024-07-05 16:16:55 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Provider } from "react-redux";
 import Body from "./components/Body";
 import store from "./utils/Store/store";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Provider store={store}>
       <Body />
+      <Footer/>
     </Provider>
   );
 }
